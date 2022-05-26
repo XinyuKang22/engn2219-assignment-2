15da1589878ee428b47aae44eebd4a110e1bb9a8
Xinyu Kang <u6120911@anu.edu.au>
2022-05-26 16:28:03 +1000

    Execution Trace DONE

--- src/emulator.c
+++ src/emulator.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Macros
 #define ADDRSPACE (1 << 16)
 #define RZ 0
 #define FL 5
 #define PC 7
+
+int disasm();
+
+struct trace
+{
+    char* disasm_str;
+    unsigned short rz;
+    unsigned short r1;
+    unsigned short r2;
+    unsigned short r3;
+    unsigned short r4;
+    unsigned short fl;
+    unsigned short pc;
+    struct trace* next;
+};
+
+typedef struct trace new_trace;
+new_trace head = {NULL, 0, 0, 0, 0, 0, 0, NULL};
+new_trace* p_tail = &head;
 
 // Global variables for ram and the register file
 unsigned short ram[ADDRSPACE];
@@ -15,19 +35,36 @@ unsigned short bp = 0;
 char s_z[2];
 char s_rd[3];
 char s_ra[3];
+char s_rb[3];
+char* disasm_str[20];
+unsigned short inst, op, z, rd, ra, rb, imm8;
+
+void save_state(unsigned short inst) {
+    new_trace* item = malloc(sizeof(new_trace));
+    char* the_disasm_str = malloc(sizeof(disasm_str));
+    strcpy(the_disasm_str, disasm_str);
+    item->disasm_str = the_disasm_str;
+    item->rz = registers[0];
+    item->r1 = registers[1];
+    item->r2 = registers[2];
+    item->r3 = registers[3];
+    item->r4 = registers[4];
+    item->fl = registers[5];
+    item->pc = registers[7];
+    p_tail->next = item;
+    p_tail = item;
+}
 
 
-void parse_inst(unsigned short inst, unsigned short* p_op, unsigned short* p_z,
-                unsigned short* p_rd, unsigned short* p_ra, unsigned short* p_rb,
-                unsigned short* p_imm8) {
-    *p_op =  inst >> 12;
-    *p_z =  (inst & 0b0000100000000000) >> 11;
-    *p_rd = (inst & 0b0000011100000000) >> 8;
-    if (*p_op < 4) {
-        *p_imm8 = inst & 0b0000000011111111;
+void parse_inst() {
+    op =  inst >> 12;
+    z =  (inst & 0b0000100000000000) >> 11;
+    rd = (inst & 0b0000011100000000) >> 8;
+    if (op < 4) {
+        imm8 = inst & 0b0000000011111111;
     } else {
-        *p_ra = (inst & 0b0000000001110000) >> 4;
-        *p_rb = inst & 0b0000000000000111;
+        ra = (inst & 0b0000000001110000) >> 4;
+        rb = inst & 0b0000000000000111;
     }
     return;
 }
@@ -36,13 +73,32 @@ void parse_inst(unsigned short inst, unsigned short* p_op, unsigned short* p_z,
 
 // FIXME: Instruction Execution (30%)
 void exec_inst() {
-    unsigned short inst = ram[registers[PC]];
+    inst = ram[registers[PC]];
     registers[PC] = registers[PC] + 1;
 
-    unsigned short op, z, rd, ra, rb, imm8;
-    parse_inst(inst, &op, &z, &rd, &ra, &rb, &imm8);
-    disasm(inst, op, z, rd, ra, rb, imm8);
+    // print the disassembled instruction
+    if (disasm() > 0)
+    {
+        printf(disasm_str);
+    }
+
+    // Trying to write the zero register will be ignored
+    if (rd == 0 && op !=  0b0101)
+    {
+        save_state(inst);
+        return;
+    }
+
+    // Trying to write/read the undefined register will be ignored
+    if (rd == 6 || ra == 6 || rb == 6)
+    {
+        save_state(inst);
+        return;
+    }
+
+    // Conditional execution on z-bit
     if ((z == 1) & (Zflag == 0)) {
+        save_state(inst);
         return;
     }
     
@@ -116,17 +172,17 @@ void exec_inst() {
         default:
             break;
     }
+    save_state(inst);
     return;
 }
 
 // FIXME: Print disassembly (15%)
-// Note: you will almost certainly need to change the arguments and/or return
-// type for your disasm function
-void disasm(unsigned short inst, unsigned short op, unsigned short z, unsigned short rd, unsigned short ra, unsigned short rb, unsigned short imm8) {
+int disasm() {
+    parse_inst();
+
     if (rd == 6 || ra == 6 || rb == 6)
     {
-        printf(".word %#X\n", inst);
-        return;
+        return sprintf(disasm_str, ".word %#X\n", inst);
     }
 
     s_z[1] = '\0';
@@ -137,7 +193,7 @@ void disasm(unsigned short inst, unsigned short op, unsigned short z, unsigned s
         s_z[0] = '\0';
     }
     
-    s_rd[3] = '\0';
+    s_rd[2] = '\0';
     switch (rd)
     {
     case 0:
@@ -158,7 +214,7 @@ void disasm(unsigned short inst, unsigned short op, unsigned short z, unsigned s
         break;
     }
 
-    s_ra[3] = '\0';
+    s_ra[2] = '\0';
     switch (ra)
     {
     case 0:
@@ -179,8 +235,7 @@ void disasm(unsigned short inst, unsigned short op, unsigned short z, unsigned s
         break;
     }
 
-    char s_rb[3];
-    s_rb[3] = '\0';
+    s_rb[2] = '\0';
     switch (rb)
     {
     case 0:
@@ -200,105 +255,85 @@ void disasm(unsigned short inst, unsigned short op, unsigned short z, unsigned s
         s_rb[1] = rb + '0';
         break;
     }
-
     
     switch (op) {
         case 0b0000:    // movl
-            // if (inst == 0){
-            //     printf(".word 0\n");
-            // } 
             switch (rd)
             {
             case 0:
-                printf(".word %i\n", inst);
-                break;
+                return sprintf(disasm_str, ".word %i\n", inst);
             case PC:
-                printf("jp%s %i\n", s_z, imm8);
-                break;
+                return sprintf(disasm_str, "jp%s %i\n", s_z, imm8);
             default:
-                printf("movl%s %02s, %i\n", s_z, s_rd, imm8);
-                break;
+                return sprintf(disasm_str, "movl%s %02s, %i\n", s_z, s_rd, imm8);
             }
-            break;
         case 0b0001:    // seth
-            printf("seth%s %02s, %i\n", s_z, s_rd, imm8);
-            break;
+            return sprintf(disasm_str, "seth%s %02s, %i\n", s_z, s_rd, imm8);
         case 0b0100:    // str
-            printf("str%s %02s, [%02s]\n", s_z, s_rd, s_ra);
-            break;
+            return sprintf(disasm_str, "str%s %02s, [%02s]\n", s_z, s_rd, s_ra);
         case 0b0101:    // ldr
             if (rd == PC)
             {
-                printf("jpm%s [%02s]\n", s_z, s_ra);
+                return sprintf(disasm_str, "jpm%s [%02s]\n", s_z, s_ra);
             } else
             {
-                printf("ldr%s %02s, [%02s]\n", s_z, s_rd, s_ra);
+                return sprintf(disasm_str, "ldr%s %02s, [%02s]\n", s_z, s_rd, s_ra);
             }
-            break;
         case 0b1000:    // add
             if (rb == 0)
             {
                 switch (rd)
                 {
                 case 0:
-                    printf("tst%s %02s\n", s_z, s_ra);
-                    break;
+                    return sprintf(disasm_str, "tst%s %02s\n", s_z, s_ra);
                 case PC:
-                    printf("jrp%s %02s\n", s_z, s_ra);
-                    break;
+                    return sprintf(disasm_str, "jrp%s %02s\n", s_z, s_ra);
                 default:
-                    printf("mov%s %02s, %02s\n", s_z, s_rd, s_ra);
-                    break;
+                    return sprintf(disasm_str, "mov%s %02s, %02s\n", s_z, s_rd, s_ra);
                 }
             } else
             {
                 if (rd == ra)
                 {
-                    printf("add%s %02s, %02s\n", s_z, s_ra, s_rb);
+                    return sprintf(disasm_str, "add%s %02s, %02s\n", s_z, s_ra, s_rb);
                 } else
                 {
-                    printf("add%s %02s, %02s, %02s\n", s_z, s_rd, s_ra, s_rb);
+                    return sprintf(disasm_str, "add%s %02s, %02s, %02s\n", s_z, s_rd, s_ra, s_rb);
                 }
             }
-            break;
         case 0b1001:    // sub
             if (rd == 0)
             {
-                printf("cmp%s %02s, %02s\n", s_z, s_ra, s_rb);
+                return sprintf(disasm_str, "cmp%s %02s, %02s\n", s_z, s_ra, s_rb);
             } else
             {
                 if (rd == ra)
                 {
-                    printf("sub%s %02s, %02s\n", s_z, s_ra, s_rb);
+                    return sprintf(disasm_str, "sub%s %02s, %02s\n", s_z, s_ra, s_rb);
                 } else
                 {
-                    printf("sub%s %02s, %02s, %02s\n", s_z, s_rd, s_ra, s_rb);
+                    return sprintf(disasm_str, "sub%s %02s, %02s, %02s\n", s_z, s_rd, s_ra, s_rb);
                 }
             }
-            break;
         case 0b1010:    // and
             if (rd == ra)
             {
-                printf("and%s %02s, %02s\n", s_z, s_ra, s_rb);
+                return sprintf(disasm_str, "and%s %02s, %02s\n", s_z, s_ra, s_rb);
             } else
             {
-                printf("and%s %02s, %02s, %02s\n", s_z, s_rd, s_ra, s_rb);
+                return sprintf(disasm_str, "and%s %02s, %02s, %02s\n", s_z, s_rd, s_ra, s_rb);
             }
-            break;
         case 0b1011:    // orr
             if (rd == ra)
             {
-                printf("orr%s %02s, %02s\n", s_z, s_ra, s_rb);
+                return sprintf(disasm_str, "orr%s %02s, %02s\n", s_z, s_ra, s_rb);
             } else
             {
-                printf("orr%s %02s, %02s, %02s\n", s_z, s_rd, s_ra, s_rb);
+                return sprintf(disasm_str, "orr%s %02s, %02s, %02s\n", s_z, s_rd, s_ra, s_rb);
             }
-            break;
         default:
-            printf(".word %i\n", inst);
-            break;
+            return sprintf(disasm_str, ".word %i\n", inst);
     }
-    return;
 }
 
 int main(int argc, char **argv) {
@@ -381,14 +416,18 @@ int main(int argc, char **argv) {
                 break;
 
             case 'r':  // Print register contents
-                printf("Zero Register:   %i\n", registers[0]);
-                printf("Register 1:      %i\n", registers[1]);
-                printf("Register 2:      %i\n", registers[2]);
-                printf("Register 3:      %i\n", registers[3]);
-                printf("Register 4:      %i\n", registers[4]);
-                printf("Flag register:   %i\n", registers[5]);
-                printf("undefined:       %i\n", registers[6]);
-                printf("Program Counter: %i\n", registers[7]);
+                printf("-----------------------\n");
+                printf("Index Mnemonic Value\n");
+                printf("-----------------------\n");
+                printf("0     rz       %-06hi\n", registers[0]);
+                printf("1     r1       %-06hi\n", registers[1]);
+                printf("2     r2       %-06hi\n", registers[2]);
+                printf("3     r3       %-06hi\n", registers[3]);
+                printf("4     r4       %-06hi\n", registers[4]);
+                printf("5     fl       %-06hi\n", registers[5]);
+                printf("6     -        -\n");
+                printf("7     pc       %-05u\n", registers[7]);
+                printf("-----------------------\n");
                 break;
 
             case 'm':;  // View memory
@@ -404,11 +443,12 @@ int main(int argc, char **argv) {
                     printf("Address       Value        Instruction\n");
                     printf("---------------------------------------------\n");
                 for (int i = start; i < end; i++) {
-                    unsigned short inst_t = ram[i];
-                    printf("0x%04X        0x%04X       ", i, inst_t);
-                    unsigned short op_t, z_t, rd_t, ra_t, rb_t, imm8_t;
-                    parse_inst(inst_t, &op_t, &z_t, &rd_t, &ra_t, &rb_t, &imm8_t);
-                    disasm(inst_t, op_t, z_t, rd_t, ra_t, rb_t, imm8_t);
+                    inst = ram[i];
+                    printf("0x%04X        0x%04X       ", i, inst);
+                    if (disasm() > 0)
+                    {
+                        printf(disasm_str);
+                    }
                 }
                     printf("-------------------------------------------\n");
                 printf("Showing memory from addresses %i to %i\n", start, end);
@@ -453,9 +493,17 @@ int main(int argc, char **argv) {
                 break;
 
             case 't':  // Print execution trace.
-
-                // FIXME: Execution Trace (10%)
-
+                printf("------------------------------------------------------------\n");
+                printf("pc    rz     r1     r2     r3     r4     fl    Instruction\n");
+                printf("------------------------------------------------------------\n");
+                new_trace* curr = head.next;
+                while (curr != NULL)
+                {
+                    printf("%-05u %-06hi %-06hi %-06hi %-06hi %-06hi %-05u %s\n", curr->pc, curr->rz, curr->r1,
+                    curr->r2, curr->r3, curr->r4, curr->fl, curr->disasm_str);
+                    curr = curr->next;
+                }
+                printf("------------------------------------------------------------\n");
                 break;
 
             default:
